1d44cb2259fd81c770567c74cd9e176dbe97d7e1
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-19 04:26:24 +0000

    adding timer1 with beeps included!

--- timer1.js
+++ timer1.js
@@ -1,2 +1,10 @@
-process.stdout.write('\x07');
+const input = process.argv.splice(2);
 
+for (let number of input) {
+  if (typeof +number === "number" && number > 0) {
+    setTimeout( () => {
+      process.stdout.write(`${number} second beep!\n`);
+      process.stdout.write('\x07');
+    }, number * 1000);
+  }
+};
